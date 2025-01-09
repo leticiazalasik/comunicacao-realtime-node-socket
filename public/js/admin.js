78717fcde6b648cf6657a3d0488126d6888edb85
Letícia Zalasik <leticiazalasik@gmail.com>
2025-01-09 20:47:36 -0300

    :bug: Fix: Correção da soma e apresentação de contagem de participantes no admin.

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,48 +1,49 @@
 let socket;
 
-function connectWebSocket(){
-    socket = new WebSocket(WS_URL);
+function connectWebSocket() {
+  socket = new WebSocket(WS_URL);
 
-    socket.addEventListener("open", handleSocketOpen);
-    socket.addEventListener("message", handleSocketMessage);
-    socket.addEventListener("error", handleSocketError);
-    socket.addEventListener("close", handleSocketClose);
+  socket.addEventListener("open", handleSocketOpen);
+  socket.addEventListener("message", handleSocketMessage);
+  socket.addEventListener("error", handleSocketError);
+  socket.addEventListener("close", handleSocketClose);
 }
 
-function handleSocketOpen(){
-    console.log("Websocket conectado");
-    socket.send(JSON.stringify({action: ACTIONS.ADMIN}));
+function handleSocketOpen() {
+  console.log("Websocket conectado");
+  socket.send(JSON.stringify({ action: ACTIONS.ADMIN }));
 }
 
-function handleSocketMessage(event){
-    const data = JSON.parse(event.data);
+function handleSocketMessage(event) {
+  const data = JSON.parse(event.data);
+  console.log("Mensagem recebida do WebSocket:", data); // Adicione este log para depuração
 
-    if (data.action === ACTIONS.CLIENT_COUNT_UPDATE){
-        updateClientCount(data.count)
-    }
+  if (data.action === ACTIONS.CLIENT_COUNT_UPDATE) {
+    updateClientCount(data.count);
+  }
 }
 
-function handleSocketError(error){
-    console.error("Erro no websocket:", error); 
+function handleSocketError(error) {
+  console.error("Erro no websocket:", error);
 }
 
-function handleSocketClose(){
-    console.log("Websocket fechado.Tentando reconectar em 5 segundos...")
-setTimeout(connectWebSocket, 5000);
+function handleSocketClose() {
+  console.log("Websocket fechado. Tentando reconectar em 5 segundos...");
+  setTimeout(connectWebSocket, 5000);
 }
 
-function updateClientCount(count){
-    document.getElementById("clientCount").innerText= count;
+function updateClientCount(count) {
+  document.getElementById("clientCount").innerText = count;
 }
 
-function generateCode(length){
-    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    let result = "";
+function generateCode(length) {
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let result = "";
 
-    for (let i = 0; i < length; i++){
-        result += characters.charAt(Math.random()*characters.length);
-    }
-    return result;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.random() * characters.length);
+  }
+  return result;
 }
 
 connectWebSocket();
@@ -50,27 +51,27 @@ connectWebSocket();
 const drawButton = document.getElementById("draw");
 const messageDiv = document.getElementById("message");
 
-drawButton.addEventListener("click", handleDrawClick); 
+drawButton.addEventListener("click", handleDrawClick);
 
-function handleDrawClick(){
-    const confirmationCode = generateCode(4);
+function handleDrawClick() {
+  const confirmationCode = generateCode(4);
 
-    if (socket.readyState === WebSocket.OPEN){
-        socket.send(
-            JSON.stringify({
-            action: ACTIONS.DRAW, 
-            code: confirmationCode,
-        })
+  if (socket.readyState === WebSocket.OPEN) {
+    socket.send(
+      JSON.stringify({
+        action: ACTIONS.DRAW,
+        code: confirmationCode,
+      })
     );
     displayConfirmationCode(confirmationCode);
-} else {
-console.warn("WebSocket não está aberto. Aguarde e tente novamente em instantes.")
-    }
+  } else {
+    console.warn("WebSocket não está aberto. Aguarde e tente novamente em instantes.");
+  }
 }
 
-function displayConfirmationCode(code){
-    messageDiv.innerText = code; 
-    messageDiv.classList.remove("hide-message");
-    messageDiv.classList.add("show-message");
-    drawButton.innerText="Sorteado!"
+function displayConfirmationCode(code) {
+  messageDiv.innerText = code;
+  messageDiv.classList.remove("hide-message");
+  messageDiv.classList.add("show-message");
+  drawButton.innerText = "Sorteado!";
 }
