b225c73d025503de4dcc714b31aa1830f272e435
Letícia Zalasik <leticiazalasik@gmail.com>
2025-01-09 16:10:12 -0300

    :sparkles: Feat: Crição de client, admin e server js, também configurado o env e mudanças no index de html.

---
+++ public/js/admin.js
@@ -0,0 +1,76 @@
+let socket;
+
+function connectWebSocket(){
+    socket = new WebSocket(WS_URL);
+
+    socket.addEventListener("open", handleSocket);
+    socket.addEventListener("message", handleSocket);
+    socket.addEventListener("error", handleSocket);
+    socket.addEventListener("close", handleSocket);
+}
+
+function handleSocketOpen(){
+    console.log("Websocket conectado");
+    socket.send(JSON.stringify({action: ACTIONS.ADMIN}));
+}
+
+function handleSocketMessage(event){
+    const data = JSON.parse(event.data);
+
+    if (data.action === ACTIONS.CLIENT_COUNT_UPDATE){
+        updateClientCount(data.count)
+    }
+}
+
+function handleSocketError(error){
+    console.error("Erro no websocket:", error); 
+}
+
+function handleSocketClose(){
+    console.log("Websocket fechado.Tentando reconectar em 5 segundos...")
+    setTimeout(connectWebSocket(), 5000);
+}
+
+function updateClientCount(){
+    document.getElementById("clientCount").innerText= count;
+}
+
+function generateCode(length){
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let result = "";
+
+    for (let i = 0; i < length; i++){
+        result += characters.charAt(Math.random()*characters.length);
+    }
+    return result;
+}
+
+connectWebSocket();
+
+const drawButton = document.getElementById("draw");
+const messageDiv = document.getElementById("message");
+
+drawButton.addEventListener("click", handleDrawClick); 
+
+function handleDrawClick(){
+    const confirmationCode = generateCode(4);
+
+    if (socket.readyState === WebSocket.OPEN){
+        socket.send(
+            JSON.stringify({
+            action: ACTIONS.DRAW, 
+            code: confirmationCode,
+        })
+    );
+    displayConfirmationCode(confirmationCode);
+} else {
+console.warn("WebSocket não está aberto. Aguarde e tente novamente em instantes.")
+    }
+}
+
+function displayConfirmationCode(code){
+    messageDiv.innerText = code; 
+    messageDiv.classList.remove("hide-message");
+    messageDiv.classList.add("show-message");
+    drawButton.innerText="Sorteado!"
+}
---
+++ public/js/client.js
@@ -0,0 +1,46 @@
+const socket = new WebSocket(WS_URL);
+
+const body = document.querySelector("body");
+const logo = document.getElementById("logo");
+const messageDiv = document.getElementById("message");
+
+socket.addEventListener("message", handleServerMessage);
+
+function handleServerMessage(event){
+    const data = JSON.parse(event.data)
+    console.log("Mensagem recebida do servidor:", data)
+
+    switch(data.status){
+        case STATUS.WIN:
+            setClientState("win", data.code);
+            break;
+            case STATUS.LOSE:
+                setClientState("lose");
+                break;
+    }
+}
+
+function setClientState(state, code = "") {
+    // Início da animação
+    body.className = "main";
+    messageDiv.classList.toggle("hide-message", true);
+    logo.classList.toggle("stop-spin", false);
+    logo.classList.toggle("spin-animation", true);
+  
+    setTimeout(() => {
+      if (state === "win") {
+        body.classList.add("win");
+        messageDiv.innerText = code;
+        vibratePhone(1000);
+      } else if (state === "lose") {
+        body.classList.add("lose");
+        messageDiv.innerText = "";
+      }
+  
+      messageDiv.classList.toggle("show-message", state === "win");
+      messageDiv.classList.toggle("hide-message", state !== "win");
+      logo.classList.toggle("spin-animation", false);
+      logo.classList.toggle("stop-spin", true);
+    }, 4100);
+  }
+  
